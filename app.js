8573ff440803d31474c8ee7b6b9573b91b90d0e0
Daniel Alberto Canaviri Mena Web Developer <canavirimenadaniel@gmail.com>
2020-12-19 21:38:45 -0400

    version-2

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
+
+/* Obtiene en la variable de nombre box un NodeList cuyos elementos son
+aquellos que en nuestro documento HTML tienen definido la clase 'box' */
 let box = document.querySelectorAll('.box');
-console.log(box);
+
+/* 
+    A los elementos que se le da clic le aplica la clase llamada 'active',
+    si el elemento ya cuenta con dicha clase entonces se la remueve.
+*/
+box.forEach( popup => popup.addEventListener('click', () => {
+    popup.classList.toggle('active');
+}));
+
+
+
+
+
+// Recordemos:
+
+/* console.log("*** Recorriendo el array con un forEach ***");
+array1.forEach( element => console.log(element)); */
